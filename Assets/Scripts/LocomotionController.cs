c8348edddd37cc2f8de0f86d9f4ec0513f688628
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-10-05 11:06:41 -0500

    Start of Locomotion Controller

---
+++ Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class LocomotionController : MonoBehaviour
+{
+
+    public LayerMask layerMask;
+
+    public GameObject targetRetical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (targetRetical == null)
+        {
+            targetRetical = BuildDebugReticule();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3? targetPos = GetNewPos();
+        if (targetPos != null)
+        {
+            targetRetical.transform.position = (Vector3)targetPos;
+        }
+        targetRetical.SetActive(targetPos != null);
+    }
+
+    Vector3? GetNewPos(){
+        
+        Vector3? newPos = null;
+        RaycastHit hit;
+
+        if (!Physics.Raycast(transform.position, transform.forward, out hit, 10f, layerMask))
+        {
+            return null;
+        }
+        if (Vector3.Distance(Vector3.up, hit.normal) > 0.2f)
+        {
+            return null;
+        }
+        newPos = hit.point;
+        return newPos;
+    }
+
+    private GameObject BuildDebugReticule()
+    {
+        GameObject retical = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        retical.SetLayerRecursively(LayerMask.NameToLayer("Ignore Raycast"));
+        retical.GetComponent<MeshRenderer>().material.color = Color.red;
+        return retical;
+    }
+}
